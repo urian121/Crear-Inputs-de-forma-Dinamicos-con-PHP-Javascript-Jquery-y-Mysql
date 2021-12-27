e715f58a912f0cec482f6388c465b92fae3ad99c
URIAN VIERA <urian1213viera@gmail.com>
2021-12-26 19:10:31 -0600

    Add files via upload

--- demo2/recib.php
+++ demo2/recib.php
@@ -2,7 +2,7 @@
 $usuario        = "root";
 $password       = "";
 $servidor       = "localhost";
-$basededatos    = "bd_stiven";
+$basededatos    = "demos";
 $con            = mysqli_connect($servidor, $usuario, $password) or die("No se ha podido conectar al Servidor");
 $db             = mysqli_select_db($con, $basededatos) or die("Upps! Error en conectar a la Base de Datos");
 
@@ -17,8 +17,11 @@ $CODE_REFERENCIA  = codAleatorio();
 
 
 for ($i=0; $i < count($ESTADO); $i++){
+
 $InserData =("INSERT INTO tb_st (PRODUCTO, NUMERO_ORDEN, ESTADO,CODE_REFERENCIA) VALUES ('".$PRODUCTO."','".$NUMERO_ORDEN[$i]."','".$ESTADO[$i]."','".$CODE_REFERENCIA."')");
 $resultadoInsertUser = mysqli_query($con, $InserData);
   
   }
+
+ header('Location: ../');
 ?>
